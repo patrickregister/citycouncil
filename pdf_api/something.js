94f39624bbdcf06a67cc2bf6bd385700722614d7
Patrick Register <register.industrial@gmail.com>
2017-04-26 00:02:27 -0400

    Update something.js

--- pdf_api/something.js
+++ pdf_api/something.js
@@ -5,22 +5,24 @@ var lineReader = require('readline').createInterface({
 
 var currentPetition = '';
 
+//create object to hold petitions
 // looks like: {
 //   "1": ["petition to destroy the roundaobut", "petition to rezone the terrace"]
 // };
 var petitionsByDistrict = {};
-
+//finds Rezoning Petition lines (These are the headings for each zoning petition)
 lineReader.on('line', function (line) {
   var title = line.match(/Rezoning Petition: (.*)/);
   if (title) {
     currentPetition = title[1];
 
   }
-
+  //finds lines containing 'Council District #'
   var district = line.match(/Council District ([1-7])/);
   if (district){
     //add item into petition that matches district
     var districtNumber = district[1];
+    //skip lines that do not contain 'Council District #'
     if(!petitionsByDistrict[districtNumber]) {
       petitionsByDistrict[districtNumber] = [];
     }
